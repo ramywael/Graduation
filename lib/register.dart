384c2c8fde5d8abc2f7695b53a312ec6b6cd17d4
ramywael <ramybakry9@gmail.com>
2024-04-23 23:06:43 +0200

    Refactor signup.dart

--- lib/register.dart
+++ lib/register.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:grad/screens/bloodreq_bloodtype_notifications_screens/blood_request.dart';
-import 'package:grad/screens/booking_donate_now/booking_screen.dart';
-import 'package:grad/screens/chatbot_profile_rate_screens/chatbot.dart';
-import 'package:grad/screens/chatbot_profile_rate_screens/rate_experience.dart';
-import 'package:grad/screens/home/user_home_page.dart';
 import 'package:grad/screens/onBoarding/onBoarding.dart';
 
 class DonationBlood extends StatelessWidget {
